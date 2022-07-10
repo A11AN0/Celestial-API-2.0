7c9344db24d60e8baf34dc5c1d1394beaf22f80c
Allan <matrixboston7613@gmail.com>
2022-07-10 15:53:51 +0800

    primary layout of index file

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 //This is the main file for the API.
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(cors());
+// app.use("/api/celestialBodies", router);
+// const db = require("./config/pool.config");
+
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
 const app = express();
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-
+const router = require("./routes/celestialbodies.routes");
 const PORT = 3007;
+
+app.listen(process.env.PORT || PORT, () => {
+    return console.log(`Express is listening at http://localhost:${PORT}`);
+});
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(process.env.PORT || PORT, () => {
-    return console.log(`Express is listening at http://localhost:${PORT}`);
+app.get("/api", (req, res) => {
+    res.send("Welcome to the Celestial API");
 });
+
+app.use("/api/celestialbodies", router);
