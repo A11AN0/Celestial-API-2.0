69ea1c9cb6ba8546804592601301675c652db769
Allan <matrixboston7613@gmail.com>
2022-07-15 01:05:32 +0800

    post route for creating new celestial body

--- routes/celestialbodies.routes.js
+++ routes/celestialbodies.routes.js
@@ -1,7 +1,7 @@
 const Router = require("express");
 const db = require("../config/pool.config");
 const celestialbodies = require("../controllers/celestialbodies.controller");
-const { findAll, find } = celestialbodies;
+const { findAll, find, create } = celestialbodies;
 const router = Router();
 
 router.get("/", findAll);
@@ -9,10 +9,10 @@ router.get("/", findAll);
 //1. Get a specific celestial body status 200:success
 router.get("/:id", find);
 
-// //2. Post (make a new student) status 201:resource created
-// router.post("/", create);
+// //2. Post (make a new celestial body) status 201:resource created
+router.post("/", create);
 
-// //3. Delete a student 204; resource deleted
+// //3. Delete a celestial body 204; resource deleted
 // router.delete("/:id", destroy);
 
 module.exports = router;
